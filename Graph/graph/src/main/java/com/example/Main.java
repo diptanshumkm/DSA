e6184e7d3052479fe8f54e5d10c3cea58e97fc55
diptanshumkm <diptanshu.uae@gmail.com>
2025-03-12 23:00:25 +0530

    Update

--- Graph/graph/src/main/java/com/example/Main.java
+++ Graph/graph/src/main/java/com/example/Main.java
@@ -1,38 +1,13 @@
 package com.example;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-
-        int n = 5; //Number of nodes
-        int adjMatrix[][] = new int[n+1][n+1];
-
-        int edges[][] = {
-            {1, 2},
-            {1, 3},
-            {2, 4},
-            {2, 5},
-            {3, 4},
-            {4, 5}
-        };
-
-        for(int[] edge: edges){
-            int u = edge[0];
-            int v = edge[1];
-            adjMatrix[u][v] = 1;
-            adjMatrix[v][u] = 1; //Graph is undirected
-
-        }
-
-
-        // Print the matrix
-        for(int i=1; i<=n; i++){
-            for(int j=1; j<=n; j++){
-                System.out.print(adjMatrix[i][j] + " ");
-            }
-            System.out.println();
-        }
-
+        int visitedArr[] = new int[10];
+        Arrays.fill(visitedArr, 0);
+        System.out.println(visitedArr);
+       
 
     }
 }
---
+++ Graph/graph/src/main/java/com/example/Traversals.java
@@ -0,0 +1,82 @@
+package com.example;
+
+import java.util.*;
+
+
+
+// Assumes the graph is connected (i.e., all nodes are reachable from node 1).
+
+public class Traversals {
+    
+
+    public static ArrayList<Integer> bfsTraversal(int nodes, ArrayList<ArrayList<Integer>> adjList){
+
+        Queue<Integer> queue = new LinkedList<>();
+        int visitedArr[] = new int[nodes+1];
+        ArrayList<Integer> answer = new ArrayList<>();
+
+        queue.add(1);
+        visitedArr[1] = 1;
+
+        while (!queue.isEmpty()) {
+            
+            int data = queue.poll();
+            answer.add(data);
+
+            ArrayList<Integer> list = adjList.get(data);
+
+            for(int i: list){
+                if(visitedArr[i] != 1){
+                    queue.add(i);
+                    visitedArr[i] = 1;
+                }
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int n = 9; // Number of nodes
+        ArrayList<ArrayList<Integer>> adjList = new ArrayList<>();
+
+        // Initialize adjacency list
+        for (int i = 0; i <= n; i++) {
+            adjList.add(new ArrayList<>());
+        }
+
+        // Add edges based on the given tree
+        adjList.get(1).add(2);
+        adjList.get(1).add(6);
+
+        adjList.get(2).add(1);
+        adjList.get(2).add(3);
+        adjList.get(2).add(4);
+
+        adjList.get(3).add(2);
+
+        adjList.get(4).add(2);
+        adjList.get(4).add(5);
+        adjList.get(4).add(8);
+
+        adjList.get(5).add(4);
+        adjList.get(5).add(8);
+
+        adjList.get(6).add(1);
+        adjList.get(6).add(7);
+        adjList.get(6).add(9);
+
+        adjList.get(7).add(6);
+
+        adjList.get(8).add(4);
+        adjList.get(8).add(5);
+
+        adjList.get(9).add(6);
+
+
+        ArrayList<Integer> result = bfsTraversal(n, adjList);
+        System.out.println(result);
+    }
+
+
+
+}
