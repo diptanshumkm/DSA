9a3576233473eb9f28c842adc1937ea2402f71a1
diptanshumkm <diptanshu.uae@gmail.com>
2025-05-09 22:51:20 +0530

    Update

--- Dynamic Programming/dp/src/main/java/com/example/Knapsack.java
+++ Dynamic Programming/dp/src/main/java/com/example/Knapsack.java
@@ -3,7 +3,6 @@ package com.example;
 public class Knapsack {
    
     public static int solve(int index, int wt, int[] weight, int[] value, int[][] dp ){
-
         //Base case
         if(index < 0 || wt == 0){return 0;}
 
@@ -34,6 +33,32 @@ public class Knapsack {
         return solve(n-1, bag_wt, weight, value, dp);
     }
 
+    public static int solveTabulation(int n, int bag_wt, int[] weight, int[] value){
+        int[][] dp = new int[n+1][bag_wt+1];
+
+        for(int j = 0; j<=bag_wt; j++){
+            dp[0][j] = 0;
+        }
+        for(int i = 0; i<=n; i++){
+            dp[i][0] = 0;
+        }
+
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=bag_wt; j++){
+                //Take
+                int pick = Integer.MIN_VALUE;
+                if (weight[i-1] <= j) {
+                    pick = value[i-1] + dp[i-1][j-weight[i-1]];
+                }
+                //Not take
+                int not_pick = dp[i-1][j];
+
+               dp[i][j] = Math.max(pick, not_pick);
+            }
+        }
+
+        return dp[n][bag_wt];
+    }
     public static void main(String[] args) {
 
         int[] weight = {1,2,4,5};
@@ -42,5 +67,6 @@ public class Knapsack {
         int n = value.length;
 
         System.out.println(solveMemoization(n, bag_wt, weight, value));
+        System.out.println(solveTabulation(n, bag_wt, weight, value));
     }
 }
