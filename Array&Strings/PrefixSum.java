cd3f5ae323719f9ddde500441f0f237c5cea2ae1
diptanshumkm <diptanshu.uae@gmail.com>
2025-06-02 20:38:37 +0530

    Update

--- Array&Strings/PrefixSum.java
+++ Array&Strings/PrefixSum.java
@@ -27,11 +27,39 @@ public class PrefixSum {
         return maxLen;
     }
 
-    public static void main(String[] args) {
-        int[] arr = {1, 2,3, 1, 1, 1,1,4,2,3};
-        int k = 3;
+    public static int optimizedSlidingWindow(int[] arr, int k){
+        int right =0;
+        int left = 0;
+        int sum = 0;
+        int maxLen = 0;
 
-        int result = longstSubArrWithSumK(arr, k);
+        while (right < arr.length) {
+            
+            sum+=arr[right];
+
+            while(sum > k) {
+                sum-=arr[left];
+                left++;
+            }
+
+            if (sum == k) {
+                maxLen = Math.max(maxLen, right-left+1);
+            }
+
+            right++;
+
+        }
+
+        return maxLen;
+    }
+
+
+
+    public static void main(String[] args) {
+        int[] arr = {1,2,1,0,1,1,0};
+        int k = 4;
+
+        int result = optimizedSlidingWindow(arr, k);
         System.out.println("Longest Subarray with Sum " + k + " = " + result);
     }
 
